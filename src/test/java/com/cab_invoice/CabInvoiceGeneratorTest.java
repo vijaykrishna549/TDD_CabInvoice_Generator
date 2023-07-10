ef3be6aa79286f2001220ba4b0b9086f5554b222
Rafe Athar <rafeatharmateen@gmail.com>
2023-07-10 21:24:29 +0530

    altered pom.xml and added test case in CabInvoiceGeneratorTest.java

--- src/test/java/com/cab_invoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cab_invoice/CabInvoiceGeneratorTest.java
@@ -1,5 +1,16 @@
 package com.cab_invoice;
 
-class CabInvoiceGeneratorTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
+class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
+    @Test
+    public void totalFare(){
+        double distance = 3.0;
+        int time = 3;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(33.0, fare, 0.0);
+    }
 }
