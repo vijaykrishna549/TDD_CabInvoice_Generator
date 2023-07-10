9a09d63664f201ca08858ed4f3a8644a4f7dccfa
Rafe Athar <rafeatharmateen@gmail.com>
2023-07-10 22:24:51 +0530

    altered CabInvoiceGeerator.java to have multiple rides and calculate total fare

--- src/main/java/com/cab_invoice/CabInvoiceGenerator.java
+++ src/main/java/com/cab_invoice/CabInvoiceGenerator.java
@@ -1,17 +1,46 @@
 package com.cab_invoice;
 
+import java.util.ArrayList;
+
 public class CabInvoiceGenerator {
     public static final double costPerKilometer = 10.0;
     public static final double costPerMinute = 1.0;
     public static final double minFare = 5.0;
 
+    double distance;
+    int time;
 
-    public double calculateFare(double distance, int time){
+    public CabInvoiceGenerator(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
 
-       double totalFare = (distance * costPerKilometer) + (time*costPerMinute);
-       if(totalFare < 5){
+    public double calculateFare(){
+
+       double fare = (this.distance * costPerKilometer) + (this.time*costPerMinute);
+       if(fare < 5){
            return minFare;
        }
-       return  totalFare;
+       return  fare;
     }
 }
+
+class Rides{
+
+    ArrayList<CabInvoiceGenerator> rides = new ArrayList<>();
+
+    public void addRide(CabInvoiceGenerator cIG){
+        this.rides.add(cIG);
+    }
+
+    public double calculateTotalFare(){
+        double totalFare=0.0;
+
+        for(CabInvoiceGenerator ride: this.rides){
+            totalFare += ride.calculateFare();
+        }
+        return totalFare;
+    }
+
+}
+
--- src/test/java/com/cab_invoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cab_invoice/CabInvoiceGeneratorTest.java
@@ -4,13 +4,14 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class CabInvoiceGeneratorTest {
-    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-
+//   CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator(3.0, 3);
     @Test
-    public void totalFare(){
-        double distance = 3.0;
-        int time = 3;
-        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+    public void fare(){
+//        double distance = 3.0;
+//        int time = 3;
+//        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        double fare = cabInvoiceGenerator.calculateFare();
         Assert.assertEquals(33.0, fare, 0.0);
     }
 }
